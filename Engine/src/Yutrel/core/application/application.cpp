2123e920b02552415477ac669c4909e75fe236fb
zerobut1 <1181948754@qq.com>
2023-06-24 11:59:35 +0800

    暂时放弃了

--- Engine/src/Yutrel/core/application/application.cpp
+++ Engine/src/Yutrel/core/application/application.cpp
@@ -4,31 +4,44 @@
 
 namespace Yutrel
 {
-
-    Application::Application(YutrelEngine *engine)
+    Application *CreateApplication()
     {
-        m_engine   = engine;
+        return new Application;
     }
 
-    void Application::run()
-    {
-        float delta_time;
-        while (m_running)
-        {
-            delta_time = m_engine->calculateDeltaTime();
-            m_engine->calculateFPS(delta_time);
-
-            tick(delta_time);
-            // m_engine->getRenderSystem()->tick(delta_time);
-
-            m_engine->getRenderSystem()->renderUI(m_ui);
-
-            m_engine->getWindowSystem()->tick();
-
-            m_engine->getWindowSystem()->setTitle(
-                std::string("Yutrel-Editor - " + std::to_string(m_engine->getFPS()) + " FPS").c_str());
-
-            m_running = !g_runtime_global_context.m_window_system->shouldClose();
-        }
-    }
 } // namespace Yutrel
+
+// #include "yutrel_pch.h"
+
+// #include "application.h"
+
+// namespace Yutrel
+// {
+
+//     Application::Application(YutrelEngine *engine)
+//     {
+//         m_engine   = engine;
+//     }
+
+//     void Application::run()
+//     {
+//         float delta_time;
+//         while (m_running)
+//         {
+//             delta_time = m_engine->calculateDeltaTime();
+//             m_engine->calculateFPS(delta_time);
+
+//             tick(delta_time);
+//             // m_engine->getRenderSystem()->tick(delta_time);
+
+//             m_engine->getRenderSystem()->renderUI(m_ui);
+
+//             m_engine->getWindowSystem()->tick();
+
+//             m_engine->getWindowSystem()->setTitle(
+//                 std::string("Yutrel-Editor - " + std::to_string(m_engine->getFPS()) + " FPS").c_str());
+
+//             m_running = !g_runtime_global_context.m_window_system->shouldClose();
+//         }
+//     }
+// } // namespace Yutrel
--- Engine/src/Yutrel/core/application/application.h
+++ Engine/src/Yutrel/core/application/application.h
@@ -1,31 +1,57 @@
 #pragma once
 
-#include "Yutrel/core/engine.h"
+#include "Yutrel/core/ecs/ecs.hpp"
 
 namespace Yutrel
 {
     class Application
     {
     public:
-        Application(YutrelEngine *engine);
+        Application()          = default;
         virtual ~Application() = default;
 
-        virtual void initialize() = 0;
-        virtual void clear()      = 0;
+        void Init();
+        void Run();
+        void Shutdown();
 
-        void run();
-
-    protected:
-        virtual void tick(float delta_time) = 0;
-
-    protected:
-        YutrelEngine *m_engine;
-
-        std::shared_ptr<WindowUI> m_ui;
+        void AddSystem();
 
     private:
-        bool m_running = true;
+        World m_world;
     };
 
-    Application *CreateApplication(YutrelEngine *engine);
+    Application *CreateApplication();
+
 } // namespace Yutrel
+
+// #pragma once
+
+// #include "Yutrel/core/engine.h"
+
+// namespace Yutrel
+// {
+//     class Application
+//     {
+//     public:
+//         Application(YutrelEngine *engine);
+//         virtual ~Application() = default;
+
+//         virtual void initialize() = 0;
+//         virtual void clear()      = 0;
+
+//         void run();
+
+//     protected:
+//         virtual void tick(float delta_time) = 0;
+
+//     protected:
+//         YutrelEngine *m_engine;
+
+//         std::shared_ptr<WindowUI> m_ui;
+
+//     private:
+//         bool m_running = true;
+//     };
+
+//     Application *CreateApplication(YutrelEngine *engine);
+// } // namespace Yutrel
